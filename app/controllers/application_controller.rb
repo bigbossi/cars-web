d85f434a7ab366f53710f6d19ef78effad370e5d
Si <le_si2003@yahoo.com>
2015-06-29 16:50:03 +0700

    clear cache if user signin/up/register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :clear_cars_list_cache
+
+
+  def clear_cars_list_cache
+    if devise_controller?
+      Rails.cache.clear 'all_cars'
+    end
+  end
+  
 end
