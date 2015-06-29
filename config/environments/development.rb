bf59791e71b4ea43a7205542218cebdfcfcfa2d0
Si <le_si2003@yahoo.com>
2015-06-29 16:25:42 +0700

    config devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,9 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  # Devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
   # Cache
   config.action_controller.perform_caching = true
   config.cache_store = :memory_store
